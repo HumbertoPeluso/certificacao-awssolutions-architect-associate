dcfc023b34e1dccead5297d05cdc0e5c5b6e95a3
HumbertoPeluso <humbertopeluso@yahoo.com.br>
2022-12-14 17:16:16 -0300

    launch template

---
+++ secao-4/launch_template.tf
@@ -0,0 +1,24 @@
+resource "aws_launch_template" "servidorweb" {
+  name = "servidorweb"
+
+  image_id = "ami-0b0dcb5067f052a63"
+
+  instance_type = "t2.micro"
+
+  key_name = "keypair-devcert"
+
+  vpc_security_group_ids = [aws_security_group.only-my-pc.id, 
+  aws_security_group.only-web-my-pc.id, 
+  data.aws_security_group.default.id
+  ]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "template-servidorweb"
+    }
+  }
+
+  user_data = filebase64("${path.module}/userdata.sh")
+}
---
+++ secao-4/sg.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "only-my-pc" {
+  name        = "only-my-pc"
+  description = "Allow ssh from my pc"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "SSH from my pc"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["189.71.121.81/32"]
+  }
+
+  tags = {
+    Name = "secao4-sg"
+  }
+}
+
+resource "aws_security_group" "only-web-my-pc" {
+  name        = "only-web-my-pc"
+  description = "Allow http from my pc"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "SSH from my pc"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["189.71.121.81/32"]
+  }
+
+  tags = {
+    Name = "secao4-sg"
+  }
+}
